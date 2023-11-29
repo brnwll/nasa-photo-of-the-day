de6e0187bc32e4a488efbb0d09d90a42bbcdd798
Brian Howell <brianhowell@pm.me>
2023-11-29 18:39:59 -0500

    Fetch NASA data and add mock data for use during development

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import mockData from '../mock/mockData'
+import { API_KEY } from '../api-key'
 
 function App() {
+  const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
+  const [data, setData] = useState(null)
+  useEffect(() => fetchMockData(), [])
+  // useEffect(() => fetchData(), [])
+
+  const fetchMockData = () => setData(mockData)
+  const fetchData = () => {
+    // TODO: Handle x-ratelimit-limit exceeded
+    // TODO: Gracefully catch error
+    axios.get(API_URL)
+      .then(res => setData(res.data))
+      .catch(err => console.error(err))
+  }
+
   return (
-    <p>
-      Read through the instructions in the README.md file to build your NASA
-      app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-    </p>
+    <div></div>
   )
 }
 
---
+++ frontend/mock/mockData.js
@@ -0,0 +1,64 @@
+const mockData = {
+    "copyright": "\nBrad Hannon\n",
+    "date": "2023-11-29",
+    "explanation": "Could there be a tornado inside another tornado? In general, no.  OK, but could there be a tornado inside a wider dust devil? No again, for one reason because tornados comes down from the sky, but dust devils rise up from the ground. What is pictured is a landspout, an unusual type of tornado known to occur on the edge of a violent thunderstorm. The featured landspout was imaged and identified in Kansas, USA, in June 2019 by an experienced storm chaser.  The real tornado is in the center, and the outer sheath was possibly created by large dust particles thrown out from the central tornado. So far, the only planet known to create tornados is Earth, although tornado-like activity has been found on the Sun and dust devils are common on Mars.   Almost Hyperspace: Random APOD Generator",
+    "hdurl": "https://apod.nasa.gov/apod/image/2311/LowerLandspout_Hannon_960.jpg",
+    "media_type": "image",
+    "service_version": "v1",
+    "title": "A Landspout Tornado over Kansas",
+    "url": "https://apod.nasa.gov/apod/image/2311/LowerLandspout_Hannon_960.jpg"
+}
+
+export default mockData
+
+/*
+// Example response data from APOS API
+{
+"data": {
+    "copyright": "\nBrad Hannon\n",
+    "date": "2023-11-29",
+    "explanation": "Could there be a tornado inside another tornado? In general, no.  OK, but could there be a tornado inside a wider dust devil? No again, for one reason because tornados comes down from the sky, but dust devils rise up from the ground. What is pictured is a landspout, an unusual type of tornado known to occur on the edge of a violent thunderstorm. The featured landspout was imaged and identified in Kansas, USA, in June 2019 by an experienced storm chaser.  The real tornado is in the center, and the outer sheath was possibly created by large dust particles thrown out from the central tornado. So far, the only planet known to create tornados is Earth, although tornado-like activity has been found on the Sun and dust devils are common on Mars.   Almost Hyperspace: Random APOD Generator",
+    "hdurl": "https://apod.nasa.gov/apod/image/2311/LowerLandspout_Hannon_960.jpg",
+    "media_type": "image",
+    "service_version": "v1",
+    "title": "A Landspout Tornado over Kansas",
+    "url": "https://apod.nasa.gov/apod/image/2311/LowerLandspout_Hannon_960.jpg"
+},
+"status": 200,
+"statusText": "",
+"headers": {
+    "content-type": "application/json",
+    "x-ratelimit-limit": "2000",
+    "x-ratelimit-remaining": "1987"
+},
+"config": {
+    "transitional": {
+        "silentJSONParsing": true,
+        "forcedJSONParsing": true,
+        "clarifyTimeoutError": false
+    },
+    "adapter": [
+        "xhr",
+        "http"
+    ],
+    "transformRequest": [
+        null
+    ],
+    "transformResponse": [
+        null
+    ],
+    "timeout": 0,
+    "xsrfCookieName": "XSRF-TOKEN",
+    "xsrfHeaderName": "X-XSRF-TOKEN",
+    "maxContentLength": -1,
+    "maxBodyLength": -1,
+    "env": {},
+    "headers": {
+        "Accept": "application/json, text/plain, *" 
+        },
+        "method": "get",
+        "url": "https://api.nasa.gov/planetary/apod?api_key=iWszNRauFl4eGidHZb85oMQV0K2Lvh4oE7Lend6u"
+    },
+    "request": {}
+}
+*/
