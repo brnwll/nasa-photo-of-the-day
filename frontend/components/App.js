8a6e459d5146022050091184e6edc94ad9153c10
Brian Howell <brianhowell@pm.me>
2023-11-29 20:21:08 -0500

    Add loading feedback component

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,24 +3,28 @@ import axios from 'axios'
 import mockData from '../mock/mockData'
 import AposPhoto from './AposPhoto'
 import AposDetails from './ApoDetails'
+import LoadingSpinner from './LoadingSpinner'
 import { API_KEY } from '../api-key'
 
 function App() {
   const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
   const [data, setData] = useState(null)
-  useEffect(() => fetchMockData(), [])
-  // useEffect(() => fetchData(), [])
 
-  const fetchMockData = () => setData(mockData)
+  const fetchMockData = () => setTimeout(() => setData(mockData), 1500)
+  useEffect(() => fetchMockData(), [])
+
   const fetchData = () => {
     // TODO: Handle x-ratelimit-limit exceeded
     // TODO: Gracefully catch error
     axios.get(API_URL)
       .then(res => setData(res.data))
-      .catch(err => console.error(err))
+      .catch(err => {
+        console.error(err)
+      })
   }
+  //useEffect(() => fetchData(), [])
 
-  console.log(data)
+  const renderSpinner = () => (<LoadingSpinner></LoadingSpinner>)
   const renderApos = () => (
     <div>
       <AposPhoto srcUrl={data.url} alt={data.title}></AposPhoto>
@@ -33,7 +37,18 @@ function App() {
     </div>
   )
 
-  return data ? renderApos() : (<></>) // TODO: Render spinner if data is null
+  return data ? renderApos() : renderSpinner()
 }
 
 export default App
+
+/*
+Stretch goal:
+Notice the optional query param date? You can pass a different date in your url 
+to get the APOD from a different date. Add a date dropdown or a date picker 
+that allows you to select a different date and see that APOD. 
+This will be quite a bit of work, but it will be a fantastic exercise to go 
+through a little more complicated logic and interaction in your app. 
+This is also a very common type of interaction, 
+so it would be good to try this out.
+*/
---
+++ frontend/components/LoadingSpinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// TODO: Make this an actual spinner during the next module
+export default function LoadingSpinner() {
+  return (
+    <div>
+      Loading photo of the day
+    </div>
+  )
+}
